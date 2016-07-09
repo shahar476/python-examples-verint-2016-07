97f477e80512db999c49118c92f2f5960bd13561
shahar476 <shahar.ron@verint.com>
2016-07-09 13:17:53 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -1,8 +1,6 @@
-"""
-Write a program that reads lines from the user
-until an empty line is inserted.
-After the user typed in an empty line,
-print all previously inserted lines in reverse
-order (from last to first)
-"""
-
+line = raw_input()
+x = ""
+while len(line) > 0:
+    x = line + "\n" + x
+    line = raw_input()
+print x
